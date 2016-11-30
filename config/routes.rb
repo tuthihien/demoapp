0b406c88f124996280f8985bb20dad571db7d931
tuthihien <muaphun1138@gmail.com>
2016-11-30 12:49:27 +0700

    fix layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-
+ get  '/help',    to: 'pages#help'
+  get  '/about',   to: 'pages#about'
+  get  '/contact', to: 'pages#contact'
 
   get '/signup' , to:'users#new'
 
