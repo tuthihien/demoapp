b9b92b7fb9aa878fcffee0db402f2b83250b7ec5
tuthihien <muaphun1138@gmail.com>
2016-12-01 09:40:31 +0700

    final fix fuck

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,33 @@ class CommentsController < ApplicationController
   def create
       @comment= current_user.comments.build(comment_params)
       if @comment.save
-      	flash[:success] ="success commented"
-      	redirect_to :back
+        flash[:success] ="success commented"
+        redirect_to :back
       else
-      	flash.now[:danger] = "error"
+        flash.now[:danger] = "error"
       end
   end
-
+def destroy 
+   @comment=Comment.find(params[:id])
+   @entry=@comment.entry
+   @comment.destroy
+    flash[:success] = "comment deleted"
+    redirect_to @entry
+end
+def edit
+  @comment=Comment.find(params[:id])
+end
+def update
+@comment=Comment.find(params[:id])
+      if @comment.update_attributes(comment_params)
+        flash[:success]="comment edited"
+        redirect_to @comment.entry
+      else
+        render 'edit'
+      end
+end  
   private
   def comment_params
-  	params.require(:comment).permit(:content, :entry_id)
+    params.require(:comment).permit(:content, :entry_id)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ApplicationRecord
-  default_scope -> {order(created_at: :desc)}
   belongs_to :user
   belongs_to :entry
+
+  default_scope -> { order(created_at: :desc) }
+
+  self.per_page = 10
+
+  # Validates
+  validates :user_id, presence: true
+  validates :entry_id, presence: true
+  validates :content, presence: true
 end
+
